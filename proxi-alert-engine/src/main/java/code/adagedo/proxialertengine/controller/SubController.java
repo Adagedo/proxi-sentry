97d30d08569ef075946262d8557e82e1b4d193f2
Adagedo solomon Israel <adagedosolomon52@gmail.com>
2026-07-12 19:05:40 +0100

    added logs where neccessary

--- proxi-alert-engine/src/main/java/code/adagedo/proxialertengine/controller/SubController.java
+++ proxi-alert-engine/src/main/java/code/adagedo/proxialertengine/controller/SubController.java
@@ -6,6 +6,7 @@ import code.adagedo.proxialertengine.dtos.SubscriptionResponse;
 import code.adagedo.proxialertengine.models.User;
 import code.adagedo.proxialertengine.service.UserService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 
+@Slf4j
 @RestController
 @CrossOrigin(
         methods = {RequestMethod.POST, RequestMethod.GET}
@@ -24,19 +26,25 @@ public class SubController {
 
     @PostMapping("/api/v1/users/subscribe")
     public ResponseEntity<SubscriptionResponse> handleSubscription(@RequestBody SubscriptionRequest request){
+
         User user = userService.registerUser(request);
+
         SubscriptionData subscriptionData = new SubscriptionData(
                 user.getEmail(),
                 user.getPhoneNumber(),
                 user.getLatitude(),
                 user.getLongitude()
         );
+
         SubscriptionResponse response = new SubscriptionResponse(
                 HttpStatusCode.valueOf(200),
                 "Welcome to ProxySentry! You have successfully subscribed to real-time proximity disaster tracking. You will now receive immediate notifications for any environmental hazards detected near your location.",
                 LocalDateTime.now(),
                 subscriptionData
         );
+
+        log.info("Registered new subscribed user {}", request.email());
+
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 }
--- proxi-alert-engine/src/main/java/code/adagedo/proxialertengine/service/UserService.java
+++ proxi-alert-engine/src/main/java/code/adagedo/proxialertengine/service/UserService.java
@@ -5,8 +5,10 @@ import code.adagedo.proxialertengine.exceptions.UserAlreadySubscribedException;
 import code.adagedo.proxialertengine.models.User;
 import code.adagedo.proxialertengine.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class UserService {
@@ -15,6 +17,7 @@ public class UserService {
     public User registerUser(SubscriptionRequest request){
         User user1 = userRepository.findByEmail(request.email());
         if (user1!= null){
+            log.info("Rejecting already subscribed user request");
             throw new UserAlreadySubscribedException("user already subscribed");
         }
 
@@ -26,7 +29,7 @@ public class UserService {
                 .longitude(request.longitude())
                 .latitude(request.latitude())
                 .build();
-
+        log.info("saving new subscribed user to db");
         return userRepository.save(user);
     }
 }
